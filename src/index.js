993c0b3c218806359e64ade5e9b163bbc9482bd3
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-10-07 18:17:43 +0300

    update: change dev environment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import express from 'express';
-import * as path from 'path';
+import { startServer } from './server.js';
 
-const app = express();
-
-const PORT = 8080;
-
-app.get('/', (_req, res) => {
-  res.sendFile('/static/index.html', { root: path.resolve('src') });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
-});
+startServer();
