1cbddf96091651de1bde34e4062a652c45a078dd
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-10-17 22:29:40 +0300

    update: add validation feat

--- src/routers/students.router.js
+++ src/routers/students.router.js
@@ -10,14 +10,38 @@ import {
 } from '../controllers/students.controller.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validate-body.middleware.js';
+import { createStudentSchema } from '../validation/students.schema.js';
+import { isValidId } from '../middlewares/is-valid-id.middleware.js';
 
 const router = Router();
 
 router.get('/students', ctrlWrapper(getAllStudentsController));
-router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
-router.post('/students', ctrlWrapper(createStudentController));
+
+router.get(
+  '/students/:studentId',
+  isValidId('studentId'),
+  ctrlWrapper(getStudentByIdController),
+);
+
+router.post(
+  '/students',
+  validateBody(createStudentSchema),
+  ctrlWrapper(createStudentController),
+);
+
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
-router.put('/student/:studentId', ctrlWrapper(upsertStudentController));
-router.patch('/student/:studentId', ctrlWrapper(patchStudentController));
+
+router.put(
+  '/student/:studentId',
+  validateBody(createStudentSchema),
+  ctrlWrapper(upsertStudentController),
+);
+
+router.patch(
+  '/student/:studentId',
+  validateBody(createStudentController),
+  ctrlWrapper(patchStudentController),
+);
 
 export default router;
